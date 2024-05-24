c8ddbf25cd24d49a77a2ab1808b7ee59e363a810
Maksym Solomkin <maksym@solomk.in>
2024-05-23 21:09:55 -0400

    Implement basic simulation

--- in-memory-set-load-testing/src/test/java/in/solomk/BasicSimulation.java
+++ in-memory-set-load-testing/src/test/java/in/solomk/BasicSimulation.java
@@ -1,54 +1,58 @@
 package in.solomk;
 
+import io.gatling.javaapi.core.ChainBuilder;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
-import static io.gatling.javaapi.core.CoreDsl.atOnceUsers;
+import java.time.Duration;
+
+import static io.gatling.javaapi.core.CoreDsl.constantUsersPerSec;
+import static io.gatling.javaapi.core.CoreDsl.exec;
 import static io.gatling.javaapi.core.CoreDsl.scenario;
 import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
 
 public class BasicSimulation extends Simulation {
 
     HttpProtocolBuilder httpProtocol = http.baseUrl("http://localhost:8080") // Here is the root for all relative URLs
             .acceptHeader("application/json"); // Here are the common headers
 
-    ScenarioBuilder addScenario = scenario("Add Scenario") // A scenario is a chain of requests and pauses
-            .exec(session -> {
-                int randomValue = new java.util.Random().nextInt(1000);
-                return session.set("value", randomValue);
-            })
-            .exec(http("Add Request")
-                    .post(session -> "/items/" + session.getInt("value"))
-                    .asJson())
-            .pause(5); // Note that Gatling has recorded real time pauses
+    ChainBuilder generateSession = exec(session -> {
+        int randomValue = new java.util.Random().nextInt(1000);
+        return session.set("value", randomValue);
+    });
 
-    ScenarioBuilder removeScenario = scenario("Remove Scenario") // A scenario is a chain of requests and pauses
-            .exec(session -> {
-                int randomValue = new java.util.Random().nextInt(1000);
-                return session.set("value", randomValue);
-            })
-            .exec(http("Remove Request")
-                    .delete(session -> "/items/" + session.getInt("value"))
-                    .asJson())
-            .pause(5); // Note that Gatling has recorded real time pauses
+    ChainBuilder addItemRequest = exec(http("Add Request")
+            .post(session -> "/items/" + session.getInt("value"))
+            .asJson());
 
-    ScenarioBuilder checkScenario = scenario("Check Scenario") // A scenario is a chain of requests and pauses
-            .exec(session -> {
-                int randomValue = new java.util.Random().nextInt(1000);
-                return session.set("value", randomValue);
-            })
-            .exec(http("Check Request")
-                    .get(session -> "/items/" + session.getInt("value"))
-                    .asJson())
-            .pause(5); // Note that Gatling has recorded real time pauses
+    ChainBuilder removeItemRequest = exec(http("Remove Request")
+            .delete(session -> "/items/" + session.getInt("value"))
+            .asJson());
+
+    ChainBuilder checkItemRequest = exec(http("Check Request")
+            .get(session -> "/items/" + session.getInt("value"))
+            .asJson()
+            .check(status().in(200, 404)));
+
+    ScenarioBuilder checkOnlyScenario = scenario("Check Only")
+            .repeat(100)
+            .on(generateSession,
+                    checkItemRequest.pause(Duration.ofMillis(10)));
+
+    ScenarioBuilder addCheckRemoveScenario = scenario("Add Check Remove")
+            .repeat(100)
+            .on(generateSession,
+                    addItemRequest.pause(Duration.ofMillis(5)),
+                    checkItemRequest.pause(Duration.ofMillis(10)),
+                    removeItemRequest.pause(Duration.ofMillis(5)));
 
 
     public BasicSimulation() {
         setUp(
-                addScenario.injectOpen(atOnceUsers(1000)),
-                removeScenario.injectOpen(atOnceUsers(1000)),
-                checkScenario.injectOpen(atOnceUsers(1000))
+                checkOnlyScenario.injectOpen(constantUsersPerSec(1000).during(60)),
+                addCheckRemoveScenario.injectOpen(constantUsersPerSec(1000).during(60))
         ).protocols(httpProtocol);
     }
 }
