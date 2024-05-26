83212e30ae3e54f4e19c9d4dd0e1b11e202a3b2c
Maksym Solomkin <maksym@solomk.in>
2024-05-26 12:41:00 -0400

    Add reflection hint

--- in-memory-set-service/src/main/java/in/solomk/inmemoryset/api/RouteConfiguration.java
+++ in-memory-set-service/src/main/java/in/solomk/inmemoryset/api/RouteConfiguration.java
@@ -1,9 +1,11 @@
 package in.solomk.inmemoryset.api;
 
+import in.solomk.inmemoryset.api.dto.OperationResponse;
 import in.solomk.inmemoryset.api.handler.AddItemHandler;
 import in.solomk.inmemoryset.api.handler.HasItemHandler;
 import in.solomk.inmemoryset.api.handler.RemoveItemHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -12,6 +14,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Slf4j
 @Configuration
+@RegisterReflectionForBinding({OperationResponse.class})
 public class RouteConfiguration {
 
     @Bean
