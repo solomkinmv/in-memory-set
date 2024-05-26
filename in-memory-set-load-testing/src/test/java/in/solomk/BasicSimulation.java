9570cf31544d78abc1cd2cc3f40ce3e869f22d35
Maksym Solomkin <maksym@solomk.in>
2024-05-26 13:11:58 -0400

    Parametrize Gatling simulation scenarios

--- in-memory-set-load-testing/src/test/java/in/solomk/BasicSimulation.java
+++ in-memory-set-load-testing/src/test/java/in/solomk/BasicSimulation.java
@@ -9,39 +9,40 @@ import java.time.Duration;
 
 import static io.gatling.javaapi.core.CoreDsl.constantUsersPerSec;
 import static io.gatling.javaapi.core.CoreDsl.exec;
+import static io.gatling.javaapi.core.CoreDsl.rampUsers;
 import static io.gatling.javaapi.core.CoreDsl.scenario;
 import static io.gatling.javaapi.http.HttpDsl.http;
 import static io.gatling.javaapi.http.HttpDsl.status;
 
 public class BasicSimulation extends Simulation {
 
-    HttpProtocolBuilder httpProtocol = http.baseUrl("http://localhost:8080") // Here is the root for all relative URLs
+    private final HttpProtocolBuilder httpProtocol = http.baseUrl(System.getProperty("host", "http://localhost:8080"))
             .acceptHeader("application/json"); // Here are the common headers
 
-    ChainBuilder generateSession = exec(session -> {
+    private final ChainBuilder generateSession = exec(session -> {
         int randomValue = new java.util.Random().nextInt(1000);
         return session.set("value", randomValue);
     });
 
-    ChainBuilder addItemRequest = exec(http("Add Request")
+    private final ChainBuilder addItemRequest = exec(http("Add Request")
             .post(session -> "/items/" + session.getInt("value"))
             .asJson());
 
-    ChainBuilder removeItemRequest = exec(http("Remove Request")
+    private final ChainBuilder removeItemRequest = exec(http("Remove Request")
             .delete(session -> "/items/" + session.getInt("value"))
             .asJson());
 
-    ChainBuilder checkItemRequest = exec(http("Check Request")
+    private final ChainBuilder checkItemRequest = exec(http("Check Request")
             .get(session -> "/items/" + session.getInt("value"))
             .asJson()
             .check(status().in(200, 404)));
 
-    ScenarioBuilder checkOnlyScenario = scenario("Check Only")
+    private final ScenarioBuilder checkOnlyScenario = scenario("Check Only")
             .repeat(100)
             .on(generateSession,
                     checkItemRequest.pause(Duration.ofMillis(10)));
 
-    ScenarioBuilder addCheckRemoveScenario = scenario("Add Check Remove")
+    private final ScenarioBuilder addCheckRemoveScenario = scenario("Add Check Remove")
             .repeat(100)
             .on(generateSession,
                     addItemRequest.pause(Duration.ofMillis(5)),
@@ -51,8 +52,8 @@ public class BasicSimulation extends Simulation {
 
     public BasicSimulation() {
         setUp(
-                checkOnlyScenario.injectOpen(constantUsersPerSec(1000).during(60)),
-                addCheckRemoveScenario.injectOpen(constantUsersPerSec(1000).during(60))
+                checkOnlyScenario.injectOpen(constantUsersPerSec(10).during(60)),
+                addCheckRemoveScenario.injectOpen(rampUsers(2000).during(60))
         ).protocols(httpProtocol);
     }
 }
